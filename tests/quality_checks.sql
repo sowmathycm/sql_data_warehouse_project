27dd3b592b3683d102ab3fe063ee594d96e059a8
sowmathycm <153798187+sowmathycm@users.noreply.github.com>
2026-06-22 20:19:41 +0530

    Add quality checks script for data validation
    Added a script for performing various quality checks on data consistency, accuracy, and standardization.

--- tests/quality_checks.sql
+++ tests/quality_checks.sql
@@ -1,4 +1,23 @@
 
+
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy,
+    and standardization across the 'silver' schemas. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
 -- Check for Nulls or Duplciates in the primary key
 -- Expectation : No result
 
